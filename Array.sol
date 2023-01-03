63b58a7de17f5adee6dc10207f9d14f837873060
tommykim <tommynara@gmail.com>
2023-01-03 20:17:17 +0900

    Create Array.sol

---
+++ Array.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.26;
+
+contract Array {
+    uint256[] public ageArray;
+  
+    function AgeLength()public view returns(uint256) {
+        return ageArray.length;
+    }
+    
+    function AgePush(uint256 _age)public{
+        ageArray.push(_age);
+    }
+    function AgeChange(uint256 _index, uint256 _age)public{
+        ageArray[_index] = _age;
+    }
+    function AgeGet(uint256 _index)public view returns(uint256){
+        return ageArray[_index];
+    }
+    
+    function Agedelete(uint256 _index)public {
+        delete ageArray[_index];
+    }
+
+
+}
