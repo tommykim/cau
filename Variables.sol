daa551a8f0429229e5107bb2b241f25b3f405106
tommykim <tommynara@gmail.com>
2023-01-03 20:16:11 +0900

    Create Variables.sol

---
+++ Variables.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.26;
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint public num = 123;
+
+    function doSomething() public {
+        // Local variables are not saved to the blockchain.
+        uint i = 456;
+        num = num + i;
+    }
+}
